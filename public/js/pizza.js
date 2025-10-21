0d589555f0cb2f6e95dec4fe40adac82958ff467
Ebtisam Jelani <93232997+Ebtisam0402@users.noreply.github.com>
2025-10-20 23:50:12 -0700

    some changes made

---
+++ public/js/pizza.js
@@ -0,0 +1,60 @@
+
+clearErrors();
+// When the form is submitted, validate
+document.getElementById('pizza-form').onsubmit = () =>{
+
+    clearErrors();
+
+//Flag variable to determine if form data is valid
+let isValid = true;
+//validiate first name
+let fname = document.getElementById('fname').value.trim();
+if(!fname ){
+document.getElementById("err-fname").style.display = "block";
+isValid = false;
+}
+//Validate last name
+let lname = document.getElementById('lname').value.trim();
+if(!lname ){
+document.getElementById("err-lname").style.display = "block";
+isValid = false;
+}
+
+//Validate email
+let email = document.getElementById('email').value.trim();
+if(!email || email.indexOf("@") === -1 ){
+document.getElementById("err-email").style.display = "block";
+isValid = false;
+}
+
+// Validate method (pickup or delivery)
+let methodButtons = document.getElementsByName("method");
+let count = 0;
+for(let i=0; i<methodButtons.lenght; i++){
+        if(methodButtons[i].checked){
+            count++;
+        }
+    }
+    if(count === 0 ){
+document.getElementById("err-method").style.display = "block";
+isValid = false;
+}
+
+//Validate Size
+let size = document.getElementById('size').value;
+if(size === "none"){
+document.getElementById("err-size").style.display = "block";
+isValid = false;
+}
+
+// return isvalid flag
+return isValid;
+
+}
+
+function clearErrors(){
+    let errors = documentByClassName("error");
+    for(let i=0; i<errors.lenght; i++){
+        errors[i].style.display = "none";
+    }
+}
